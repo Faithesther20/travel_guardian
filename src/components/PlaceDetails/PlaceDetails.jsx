69fd41e0b698e82416aab642ce2104048a54d609
Esther Iyege <faithesther20@gmail.com>
2022-05-18 06:45:02 +0100

    latest details update

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,12 +1,65 @@
 import React from "react";
-import useStyles from './styles';
-import {Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from "@material-ui/core"
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from "@material-ui/core"
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PhoneIcon from '@material-ui/icons/Phone';
-import  Rating   from "@material-ui/lab/Rating";
-const PlaceDetails = ({ place }) =>{
-    return(
-        <h1>{place.name}</h1>
+import Rating from "@material-ui/lab/Rating";
+import useStyles from './styles';
+import { mergeClasses } from "@material-ui/styles";
+import { LaptopWindows } from "@material-ui/icons";
+
+const PlaceDetails = ({ place }) => {
+    const classes = useStyles();
+    return (
+        <Card elevation={6}>
+            <CardMedia
+                style={{ height: 350 }}
+                image={place.photo ? place.photo.images.large.url : 'https://media.istockphoto.com/photos/two-empty-wine-glasses-sitting-in-a-restaurant-on-a-warm-sunny-picture-id1018141890?k=20&m=1018141890&s=612x612&w=0&h=uMDP00MMIhlwQE77EEcoelc2oSKBT_B6avaXqtxgiow='}
+                title={place.name}
+            />
+            <CardContent>
+                <Typography gutterBottom varient="h5">{place.name}</Typography>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Price</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="subtitle1">Ranking</Typography>
+                    <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+                </Box>
+                {place?.awards?.map((award) => (
+                    <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+                        <img src={award.images.small} alt={award.display_name} />
+                        <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+                    </Box>
+                ))}
+                {place?.cuisine?.map(({ name }) => (
+                    <Chip key={name} size="small" label={name} className={classes.chip} />
+                ))}
+                {place?.address && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.subtitle}>
+                     <LocationOnIcon/> {place.address}
+                    </Typography>
+                )}
+                {place?.phone && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.spacing}>
+                     <PhoneIcon/> {place.phone}
+                    </Typography>
+                )}
+                <CardActions>
+                    <Button size="small" className={classes.textColor} onClick={()=>window.open(place.web_url, '_blank')}>
+                      Travel Guardian
+                    </Button>
+                </CardActions>
+                <CardActions>
+                    <Button size="small" className={classes.textColor} onClick={()=>window.open(place.web_url, '_blank')}>
+                      Travel Guardian
+                    </Button>
+                    <Button size="small" className={classes.textColor} onClick={()=>window.open(place.website, '_blank')}>
+                      Website
+                    </Button>
+                </CardActions>
+            </CardContent>
+        </Card>
     );
 }
 
